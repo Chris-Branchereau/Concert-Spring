315efe6afe480060df57e8498f2c7d653a227890
60127@etu.he2b.be <60127@etu.he2b.be>
2025-01-20 16:37:31 +0100

    Etape 7 - Requête personnalisée

--- src/main/java/be/he2b/concerts/model/repositories/VenueDB.java
+++ src/main/java/be/he2b/concerts/model/repositories/VenueDB.java
@@ -1,9 +1,10 @@
 package be.he2b.concerts.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import be.he2b.concerts.model.entities.Venue;
 
 public interface VenueDB extends CrudRepository<Venue,Long>{
-
+    List<Venue> findAllVenueByCapacityGreaterThanEqual(int capacity);
 }
